d4cef9cbe472ae5e43833b594358ebaa0b6cece1
Mac-Arthur <macarthur83@yahoo.com>
2025-07-12 17:01:07 +0000

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'learnTailwind';
+  menuOpen = false;
+  menuOpenV2 = false;
+  sidebarOpen = false;
+  sidebarOpenV2 = false;
 }
